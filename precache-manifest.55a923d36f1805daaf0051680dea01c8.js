b88973406c952866de547b27c1b18e500d2742df
Pawan V <pawangohan98@gmail.com>
2020-04-23 20:42:42 +0530

    Updates

---
+++ precache-manifest.55a923d36f1805daaf0051680dea01c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748807b2b37a8c2a26f8e9805e6eee9a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "cac376b8e24f29781954",
+    "url": "/robofriends/static/css/main.b8ea15c7.chunk.css"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/robofriends/static/js/2.a10038f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.a10038f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cac376b8e24f29781954",
+    "url": "/robofriends/static/js/main.97544f76.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  },
+  {
+    "revision": "8c519a69d795dc8fbd0e33d460b3b590",
+    "url": "/robofriends/static/media/HYTO (fonds).8c519a69.woff"
+  }
+]);
--- src/Scroll.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Scroll =(props) =>{
-	return(
-	<div className="ba bw3 b--lightest-blue" style={{overflowY: 'scroll',height: '800px'}}>
-	 {props.children}
-	</div>);
-};
-export default Scroll;
